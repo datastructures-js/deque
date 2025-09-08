657e6949d6ba6127c9dcfd735dc2afec18f8ce11
eyas-ranjous <eyas.ranjous@gmail.com>
2025-09-08 00:45:05 -0700

    fix null types

--- src/deque.d.ts
+++ src/deque.d.ts
@@ -1,11 +1,11 @@
 export class Deque<T> {
   constructor(elements?: T[]);
-  pushFront(element: T): Deque<T>;
-  pushBack(element: T): Deque<T>;
-  popFront(): T;
-  popBack(): T;
+  pushFront(element: T): Deque<T> | null;
+  pushBack(element: T): Deque<T> | null;
+  popFront(): T | null;
+  popBack(): T | null;
   front(): T;
-  back(): T;
+  back(): T | null;
   toArray(): T[];
   isEmpty(): boolean;
   size(): number;
