42d119d1e4d72f21a3c801dd65b09406b43c3907
eyas <eyas.ranjous@gmail.com>
2025-09-08 00:48:10 -0700

    return null type (#12)

--- src/deque.d.ts
+++ src/deque.d.ts
@@ -2,10 +2,10 @@ export class Deque<T> {
   constructor(elements?: T[]);
   pushFront(element: T): Deque<T>;
   pushBack(element: T): Deque<T>;
-  popFront(): T;
-  popBack(): T;
-  front(): T;
-  back(): T;
+  popFront(): T | null;
+  popBack(): T | null;
+  front(): T | null;
+  back(): T | null;
   toArray(): T[];
   isEmpty(): boolean;
   size(): number;
